adad90a9de769690eb61587c5850a2a6fc7f3dd7
Sydney Escobar <se176171@umconnect.umt.edu>
2024-09-24 12:20:48 -0600

    Added comment

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import home_page_view,AboutPageView
 
 urlpatterns = [
+    # To prevent errors if file names are changed 
      path("about/", AboutPageView.as_view(), name="about"), 
     path("", home_page_view, name="home"), 
 ]
